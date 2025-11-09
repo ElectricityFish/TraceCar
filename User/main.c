39a1c1b27f18e5c4b059fbe2b2b03419656d3e16
ElectricityFish <ajy18991073299@outlook.com>
2025-11-09 19:17:28 +0800

    成功配置了编码器

--- User/main.c
+++ User/main.c
@@ -7,10 +7,8 @@
 #include "Motor.h"
 #include "Encoder.h"
 #include "Serial.h"
-#include "AD.h"
 #include "Sensor.h"
 #include "RotateEncoder.h"
-
 
 float kp=1.0,ki=1.0,kd=1.0;
 int16_t DirectSpeed=250,MSmallTurnSpeed=180,MLargeTurnSpeed=120;//制动速度
@@ -24,16 +22,11 @@ int main()
 	Motor_Init();
 	Sensor_Init();
 	LED_Init();
-	AD_Init();
 	Encoder_Init();
 	RotateEncoder_Init();
 	Timer_Init();//一般定时器在最后初始化
 	uint8_t Menu_Indxe=0;
-	float AD_Value=0;//获得初始AD值
-	float AD_PIDDebugChange=0;
-	float AD_PIDDebug=0;//用于调试kp,ki,kd
-	int16_t AD_SpeedDebug=0;//用于调试速度
-	int16_t AD_SpeedDebugChange=0;//用于调试速度
+	
 
 
 	while(1)
@@ -44,9 +37,9 @@ int main()
 		if(State==1)OLED_Printf(0,0,OLED_8X16,"RUN    ");
 		OLED_Printf(0,20,OLED_6X8,"kp=%.2f  ki=%.2f",kp,ki);
 		OLED_Printf(0,30,OLED_6X8,"kd=%.2f",kd);
-		OLED_Printf(60,30,OLED_6X8,"DiS=%d",DirectSpeed);
-		OLED_Printf(0,40,OLED_6X8,"MSTS=%d  MLTS=%d",MSmallTurnSpeed,MLargeTurnSpeed);
-		OLED_Printf(0,50,OLED_6X8,"STS=%d  LTS=%d",SmallTuenSpeed,LargeTunrSpeed);
+		OLED_Printf(60,30,OLED_6X8,"DiS=%04d",DirectSpeed);
+		OLED_Printf(0,40,OLED_6X8,"MSTS=%04d  MLTS=%04d",MSmallTurnSpeed,MLargeTurnSpeed);
+		OLED_Printf(0,50,OLED_6X8,"STS=%04d  LTS=%04d",SmallTuenSpeed,LargeTunrSpeed);
 		OLED_Update();
 		if(State==0&&KeyNum==2)//在调试模式下
 		{
@@ -57,19 +50,11 @@ int main()
 				if(Menu_Indxe==1)OLED_Printf(60,0,OLED_8X16,"kp  ");
 				if(Menu_Indxe==2)OLED_Printf(60,0,OLED_8X16,"ki  ");
 				if(Menu_Indxe==3)OLED_Printf(60,0,OLED_8X16,"kd  ");
-				//消除刚进入菜单时的赋值
-				uint8_t count=0;
 				while(Key_GetNum()==0)//没有按键按下就进行调参
 				{
-					count++;
-					AD_PIDDebugChange=AD_PIDDebug;
-					AD_Value=(AD_GetValue_Median()*0.2+AD_Value*0.8);
-					AD_PIDDebug=(AD_Value/4095)*3.0;
-					AD_PIDDebugChange=AD_PIDDebug-AD_PIDDebugChange;
-					if(count<100)AD_PIDDebugChange=0;
-					if(Menu_Indxe==1)kp+=RotateEncoder_Get();
-					if(Menu_Indxe==2)ki+=RotateEncoder_Get();
-					if(Menu_Indxe==3)kd+=RotateEncoder_Get();
+					if(Menu_Indxe==1)kp+=(float)RotateEncoder_Get()/100;
+					if(Menu_Indxe==2)ki+=(float)RotateEncoder_Get()/100;
+					if(Menu_Indxe==3)kd+=(float)RotateEncoder_Get()/100;
 					OLED_Printf(0,20,OLED_6X8,"kp=%.2f  ki=%.2f",kp,ki);
 					OLED_Printf(0,30,OLED_6X8,"kd=%.2f",kd);
 					OLED_Update();
@@ -84,24 +69,16 @@ int main()
 				if(Menu_Indxe==6)OLED_Printf(60,0,OLED_8X16,"MLTS  ");
 				if(Menu_Indxe==7)OLED_Printf(60,0,OLED_8X16,"STS  ");
 				if(Menu_Indxe==8)OLED_Printf(60,0,OLED_8X16,"LTS  ");
-				//消除刚进入菜单时的赋值
-				uint8_t count=0;
 				while(Key_GetNum()==0)//没有按键按下就进行调参
 				{
-					count++;
-					AD_SpeedDebugChange=AD_SpeedDebug;
-					AD_Value=(AD_GetValue_Median()*0.1+AD_Value*0.8);
-					AD_SpeedDebug=(AD_Value/4095)*200;
-					AD_SpeedDebugChange=AD_SpeedDebug-AD_SpeedDebugChange;
-					if(count<100)AD_SpeedDebugChange=0;
-					if(Menu_Indxe==4)DirectSpeed+=AD_SpeedDebugChange;
-					if(Menu_Indxe==5)MSmallTurnSpeed+=RotateEncoder_Get();
-					if(Menu_Indxe==6)MLargeTurnSpeed+=RotateEncoder_Get();
-					if(Menu_Indxe==7)SmallTuenSpeed+=RotateEncoder_Get();
-					if(Menu_Indxe==8)LargeTunrSpeed+=RotateEncoder_Get();
-					OLED_Printf(60,30,OLED_6X8,"DiS=%d",DirectSpeed);
-					OLED_Printf(0,40,OLED_6X8,"MSTS=%d  MLTS=%d",MSmallTurnSpeed,MLargeTurnSpeed);
-					OLED_Printf(0,50,OLED_6X8,"STS=%04d  LTS=%d",SmallTuenSpeed,LargeTunrSpeed);
+					if(Menu_Indxe==4)DirectSpeed+=RotateEncoder_Get()*5;
+					if(Menu_Indxe==5)MSmallTurnSpeed+=RotateEncoder_Get()*5;
+					if(Menu_Indxe==6)MLargeTurnSpeed+=RotateEncoder_Get()*5;
+					if(Menu_Indxe==7)SmallTuenSpeed+=RotateEncoder_Get()*5;
+					if(Menu_Indxe==8)LargeTunrSpeed+=RotateEncoder_Get()*5;
+					OLED_Printf(60,30,OLED_6X8,"DiS=%04d",DirectSpeed);
+					OLED_Printf(0,40,OLED_6X8,"MSTS=%04d  MLTS=%04d",MSmallTurnSpeed,MLargeTurnSpeed);
+					OLED_Printf(0,50,OLED_6X8,"STS=%04d  LTS=%04d",SmallTuenSpeed,LargeTunrSpeed);
 					OLED_Update();
 					
 				}
