5ba99454133fd11dfafe3320f476ce5c181fabcb
ElectricityFish <ajy18991073299@outlook.com>
2025-11-11 15:02:26 +0800

    配置了PID

--- Hardware/Motor.h
+++ Hardware/Motor.h
@@ -5,5 +5,5 @@ void Motor_Init(void);
 void Motor_SetPWM_Left1(int8_t Speed);
 void Motor_SetPWM_Left2(int8_t Speed);
 void Motor_SetPWM_Right1(int8_t Speed);
-void Motor_SetPWM_Right1(int8_t Speed);
+void Motor_SetPWM_Right2(int8_t Speed);
 #endif
