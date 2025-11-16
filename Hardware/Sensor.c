01a5d4d39b214ee4184947027c87f34b0b5802e3
ElectricityFish <ajy18991073299@outlook.com>
2025-11-16 19:04:28 +0800

    配置了一些行驶状态

--- Hardware/Sensor.c
+++ Hardware/Sensor.c
@@ -1,5 +1,24 @@
 #include "stm32f10x.h"                  // Device header
 
+//宏定义传感器返回值
+#define Sensor_Left1 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_12)
+#define Sensor_Left2 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_13)
+#define Sensor_Right1 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_14)
+#define Sensor_Right2 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_15)
+
+//宏定义行驶状态
+#define Static 0                        //等待 
+#define Direct 1                        //直行
+#define LeftExcursion_Silght 2          //左轻微偏移
+#define LeftExcursion_Large 3           //左大偏移
+#define RightExcursion_Silght 4         //右轻微偏移
+#define RightExcursion_Large 5          //右大偏移
+#define LeftTurn_Arched 6               //左圆角转弯
+#define LeftTurn_Vertical 7             //左直角转弯
+#define RightTurn_Arched 8              //右圆角转弯
+#define RightTurn_Vertical 9            //右直角转弯
+
+
 
 void Sensor_Init(void)
 {
@@ -10,6 +29,33 @@ void Sensor_Init(void)
     GPIO_InitStruct.GPIO_Pin=GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
     GPIO_InitStruct.GPIO_Speed=GPIO_Speed_50MHz;
     GPIO_Init(GPIOB,&GPIO_InitStruct);
+
+}
+
+
+//循迹逻辑
+uint8_t  Trace_GetState(void)
+{
+    //等待
+    if(Sensor_Left2==0&&Sensor_Right2==0&&Sensor_Left1==0&&Sensor_Right1==0)return Static;
+    //直行
+    if((Sensor_Left2==1&&Sensor_Right2==1)&&((Sensor_Left1==1&&Sensor_Right1==1)||(Sensor_Left1==0&&Sensor_Right1==0)))return Direct;
+    //偏移校准
+    if((Sensor_Left1==0&&Sensor_Right2==1)&&(Sensor_Left1==0&&Sensor_Right1==0))return LeftExcursion_Silght;
+    if((Sensor_Left1==0&&Sensor_Right2==0)&&(Sensor_Left1==0&&Sensor_Right1==1))return LeftExcursion_Large;
+
+    if((Sensor_Left1==0&&Sensor_Right2==1)&&(Sensor_Left1==0&&Sensor_Right1==0))return RightExcursion_Silght;
+    if((Sensor_Left1==0&&Sensor_Right2==0)&&(Sensor_Left1==0&&Sensor_Right1==1))return RightExcursion_Large;
+    //转向
+    if((Sensor_Left2==1&&Sensor_Left1==1)&&(Sensor_Right2==0&&Sensor_Right1==0))return LeftTurn_Arched;
+    if((Sensor_Left2==1&&Sensor_Left1==1)&&(Sensor_Right2==1&&Sensor_Right1==0))return LeftTurn_Vertical;
+
+    if((Sensor_Left2==0&&Sensor_Left1==0)&&(Sensor_Right2==1&&Sensor_Right1==1))return RightTurn_Arched;
+    if((Sensor_Left2==1&&Sensor_Left1==0)&&(Sensor_Right2==1&&Sensor_Right1==1))return RightTurn_Vertical;
+
+
+    //其它情况待定
+    return Static;
 
 }
 
