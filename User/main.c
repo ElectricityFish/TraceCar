3024dfbbcd7df67bb6b9e3dc8784df3c4b575f3a
ElectricityFish <ajy18991073299@outlook.com>
2025-11-18 20:07:32 +0800

    基本完成

--- User/main.c
+++ User/main.c
@@ -11,10 +11,10 @@
 
 
 int8_t DirectOut=100;
-int8_t LeftExcursion_Silght_Out,LeftExcursion_Large_Out;
-int8_t RightExcursion_Silght_Out,RightExcursion_Large_Out;
-int8_t LeftTurn_Arched_Out,LeftTurn_Vertical_Out;
-int8_t RightTurn_Arched_Out,RightTurn_Vertical_Out;
+int8_t LeftExcursion_Silght_Out=100,LeftExcursion_Large_Out=85;
+int8_t RightExcursion_Silght_Out=100,RightExcursion_Large_Out=85;
+int8_t LeftTurn_Arched_Out=80,LeftTurn_Vertical_Out=70;
+int8_t RightTurn_Arched_Out=80,RightTurn_Vertical_Out=70;
 uint8_t KeyNum,State;
 uint8_t CarState;
 
@@ -32,17 +32,24 @@ int main()
 	
 
 
-	//Motor_SetPWM_Left1(100);
-	//Motor_SetPWM_Left2(100);
-	//Motor_SetPWM_Right1(100);
-	//Motor_SetPWM_Right2(100);
+	//Motor_SetPWM_Left1(40);
+	//Motor_SetPWM_Left2(40);
+	//Motor_SetPWM_Right1(40);
+	//Motor_SetPWM_Right2(40);
 
 	while(1)
 	{
 		KeyNum=Key_GetNum();
 		if(KeyNum==1)State=!State;
-		if(State==0)OLED_Printf(0,0,OLED_8X16,"Debug                         ");
-		if(State==1)OLED_Printf(0,0,OLED_8X16,"RUN                           ");
+		if(State==0)
+		{
+			Motor_SetPWM_Left1(0);
+			Motor_SetPWM_Left2(0);
+			Motor_SetPWM_Right1(0);
+			Motor_SetPWM_Right2(0);
+			OLED_Printf(0,0,OLED_8X16,"Debug                                    ");
+		}
+		if(State==1)OLED_Printf(0,0,OLED_8X16,"RUN                                      ");
 		/********************************************* 
 		 * 
 		 * 
@@ -51,13 +58,14 @@ int main()
 		 * 
 		 * 
 		*********************************************/
-		OLED_Printf(0,20,OLED_6X8,"LESO=%04d  LELO=%04d",LeftExcursion_Silght_Out,LeftExcursion_Large_Out);
-		OLED_Printf(0,30,OLED_6X8,"RESO=%04d RELO=%04d",RightExcursion_Silght_Out,RightExcursion_Large_Out);
-		OLED_Printf(0,40,OLED_6X8,"LTAO=%04d  LTVO=%04d",LeftTurn_Arched_Out,LeftTurn_Vertical_Out);
-		OLED_Printf(0,50,OLED_6X8,"RTAO=%04d  RTVO=%04d",RightTurn_Arched_Out,RightTurn_Vertical_Out);
+		OLED_Printf(0,20,OLED_6X8,"LESO=%03d  LELO=%03d",LeftExcursion_Silght_Out,LeftExcursion_Large_Out);
+		OLED_Printf(0,30,OLED_6X8,"RESO=%03d RELO=%03d",RightExcursion_Silght_Out,RightExcursion_Large_Out);
+		OLED_Printf(0,40,OLED_6X8,"LTAO=%03d  LTVO=%03d",LeftTurn_Arched_Out,LeftTurn_Vertical_Out);
+		OLED_Printf(0,50,OLED_6X8,"RTAO=%03d  RTVO=%03d",RightTurn_Arched_Out,RightTurn_Vertical_Out);
 		OLED_Update();
 		if(State==0&&KeyNum==2)//在调试模式下
 		{
+			
 			Menu_Indxe++;
 			if(Menu_Indxe>=9)Menu_Indxe=1;//一共8个可调参数
 
@@ -99,11 +107,171 @@ int main()
 }
 }
 
+void Motor_Set(void);
+
 void TIM1_UP_IRQHandler(void)
 {
+	static uint8_t Count;
 	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
 	{
+		
 		Key_Tick();
+		if(State==1)
+		{
+			Count++;
+			if(Count>=40)
+			{
+				Count=0;
+				CarState=Trace_GetState();
+				Motor_Set();
+			}
+		}
 		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
 	}
 }
+
+
+void Motor_Set(void)
+{
+	switch (CarState)
+	{
+		case Static:
+		{
+			OLED_Printf(60,0,OLED_6X8,"Static           ");
+			OLED_Update();
+			Motor_SetPWM_Left1(0);
+			Motor_SetPWM_Left2(0);
+			Motor_SetPWM_Right1(0);
+			Motor_SetPWM_Right2(0);
+			break;
+		}
+		case Direct:
+		{
+			OLED_Printf(60,0,OLED_6X8,"Direct          ");
+			OLED_Update();
+			Motor_SetPWM_Left1(100);
+			Motor_SetPWM_Left2(100);
+			Motor_SetPWM_Right1(100);
+			Motor_SetPWM_Right2(100);
+			break;
+		}
+		case LeftExcursion_Silght:
+		{
+			OLED_Printf(60,0,OLED_6X8,"LeftExcur_Silght             ");
+			OLED_Update();
+			Motor_SetPWM_Left1(LeftExcursion_Silght_Out);
+			Motor_SetPWM_Left2(LeftExcursion_Silght_Out);
+			Motor_SetPWM_Right1(70);
+			Motor_SetPWM_Right2(70);
+			break;
+		}
+		case LeftExcursion_Large:
+		{
+			OLED_Printf(60,0,OLED_6X8,"LeftExcu_Large        ");
+			OLED_Update();
+			Motor_SetPWM_Left1(LeftExcursion_Large_Out);
+			Motor_SetPWM_Left2(LeftExcursion_Large_Out);
+			Motor_SetPWM_Right1(50);
+			Motor_SetPWM_Right2(50);
+			break;
+		}
+		case RightExcursion_Silght:
+		{
+			OLED_Printf(60,0,OLED_6X8,"RightExcu_Silght         ");
+			OLED_Update();
+			Motor_SetPWM_Left1(70);
+			Motor_SetPWM_Left2(70);
+			Motor_SetPWM_Right1(RightExcursion_Silght_Out);
+			Motor_SetPWM_Right2(RightExcursion_Silght_Out);
+			break;
+		}
+		case RightExcursion_Large:
+		{
+			OLED_Printf(60,0,OLED_6X8,"RightExcu_Large");
+			OLED_Update();
+			Motor_SetPWM_Left1(50);
+			Motor_SetPWM_Left2(50);
+			Motor_SetPWM_Right1(RightExcursion_Large_Out);
+			Motor_SetPWM_Right2(RightExcursion_Large_Out);
+			break;
+		}
+		case LeftTurn_Arched:
+		{
+			OLED_Printf(60,0,OLED_6X8,"LeftTurn_Arched         ");
+			OLED_Update();
+			Motor_SetPWM_Left1(50);
+			Motor_SetPWM_Left2(50);
+			Motor_SetPWM_Right1(LeftTurn_Arched);
+			Motor_SetPWM_Right2(LeftTurn_Arched);
+			break;
+		}
+		case LeftTurn_Vertical:
+		{
+			OLED_Printf(60,0,OLED_6X8,"LeftTurn_Vertical          ");
+			OLED_Update();
+			Motor_SetPWM_Left1(35);
+			Motor_SetPWM_Left2(35);
+			Motor_SetPWM_Right1(LeftTurn_Vertical_Out);
+			Motor_SetPWM_Right2(LeftTurn_Vertical_Out);
+			break;
+		}
+		case RightTurn_Arched:
+		{
+			OLED_Printf(60,0,OLED_6X8,"RightTurn_Arched      ");
+			OLED_Update();
+			Motor_SetPWM_Left1(RightTurn_Arched);
+			Motor_SetPWM_Left2(RightTurn_Arched);
+			Motor_SetPWM_Right1(50);
+			Motor_SetPWM_Right2(50);
+			break;
+		}
+		case RightTurn_Vertical:
+		{
+			OLED_Printf(60,0,OLED_6X8,"RightTurn_Vertical     ");
+			OLED_Update();
+			Motor_SetPWM_Left1(RightTurn_Vertical_Out);
+			Motor_SetPWM_Left2(RightTurn_Vertical_Out);
+			Motor_SetPWM_Right1(35);
+			Motor_SetPWM_Right2(35);
+			break;
+		}
+		default :
+		{
+			OLED_Printf(60,0,OLED_6X8,"Other         ");
+			OLED_Update();
+			Motor_SetPWM_Left1(0);
+			Motor_SetPWM_Left2(0);
+			Motor_SetPWM_Right1(0);
+			Motor_SetPWM_Right2(0);
+		}
+		
+
+
+
+
+	}
+	
+	
+}
+/*
+int8_t DirectOut=100;
+int8_t LeftExcursion_Silght_Out,LeftExcursion_Large_Out;
+int8_t RightExcursion_Silght_Out,RightExcursion_Large_Out;
+int8_t LeftTurn_Arched_Out,LeftTurn_Vertical_Out;
+int8_t RightTurn_Arched_Out,RightTurn_Vertical_Out;
+uint8_t KeyNum,State;
+uint8_t CarState;
+*/
+
+/*
+#define Static 0                        //等待 
+#define Direct 1                        //直行
+#define LeftExcursion_Silght 2          //左轻微偏移
+#define LeftExcursion_Large 3           //左大偏移
+#define RightExcursion_Silght 4         //右轻微偏移
+#define RightExcursion_Large 5          //右大偏移
+#define LeftTurn_Arched 6               //左圆角转弯
+#define LeftTurn_Vertical 7             //左直角转弯
+#define RightTurn_Arched 8              //右圆角转弯
+#define RightTurn_Vertical 9            //右直角转弯
+*/
